f4b956835486096ae4ed879e12fd6d4577add882
Mathieu <brrs.mathieu@gmail.com>
2025-11-19 12:05:30 -0500

    modification cors

--- server.js
+++ server.js
@@ -10,12 +10,13 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
+const allowedOrigin = process.env.FRONTEND_URL;
 
 const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: allowedOrigin,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
